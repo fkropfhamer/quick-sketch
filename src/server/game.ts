4b0aca568e9a7890370cd15238ab55893c678e65
fabi <fabian.kropfhamer@gmail.com>
2020-01-30 10:23:38 +0100

    added timer

--- src/server/game.ts
+++ src/server/game.ts
@@ -10,13 +10,14 @@ export default class Game {
     drawingPlayer: User;
     wordToDraw: string = "test"
     drawingCount: number;
+    timer: number;
 
     constructor(player: User) {
         this.players = [player];
         this.count = 0;
         this.drawingCount = 0;
         this.drawing = [];
-        
+        this.timer = 0;
         this.start();
     }
 
@@ -28,13 +29,23 @@ export default class Game {
     loop() {
         this.count += 1 
         this.count %= Config.DRAWTIME;
-        if (this.count === 0 && this.players.length > 0) {
-            this.drawingCount += 1
-            this.drawingCount %= this.players.length
-            this.drawingPlayer = this.players[this.drawingCount];
-            this.drawingPlayer.notifyDrawing();
+        if (this.count % (20) === 0) {
+            this.timer += 1;
+            console.log(this.timer);
+        }
+        if (this.count === 0) {
+            
         } 
         this.players.forEach(player => player.notifyUpdate(this.players, this.count, this.drawing, this.drawingPlayer, this.wordToDraw));
+    }
+
+    nextRound() {
+        this.timer = 0;
+        this.drawing = [];
+        this.drawingCount += 1
+        this.drawingCount %= this.players.length
+        this.drawingPlayer = this.chooseDrawingPlayer(this.drawingCount)
+        this.drawingPlayer.notifyDrawing();
     }
 
     draw(id: string, drawing: Drawing) {
@@ -45,5 +56,16 @@ export default class Game {
 
     removeUser(user: User) {
         this.players = this.players.filter(player => !Object.is(player, user));
+        if (this.players.length === 0) {
+            clearInterval(this.interval);
+        } 
+    }
+
+    chooseDrawingPlayer(drawingCount: number): User {
+        console.log(drawingCount);
+        if(this.players.length > 1) {
+            return this.players[drawingCount];
+        }
+        return this.players[0];
     }
 }
